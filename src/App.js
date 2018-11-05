b11d9be6cf490911eccd1f8dd98b277e699970b0
ZUberMench <zbrandon95@gmail.com>
2018-11-04 20:49:52 -0800

    Succesfully puts image into S3 bucket

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ Auth.currentAuthenticatedUser()
     .then(user => console.log(user))
     .catch(err => console.log(err));
 
-
 class App extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
       signOut = () => {
         Auth.signOut()
           .then(data => console.log(data))
@@ -20,14 +24,28 @@ class App extends Component {
       }
       onChange(e) {
           const file = e.target.files[0];
-          Storage.put('example.png', file, {
-              contentType: 'image/png'
+          Storage.put('example2.png', file, {
+              contentType: 'image/png',  //,
+              bucket: 'myapp-20181030214040-deployment'
           })
           .then (result => console.log(result))
           .catch(err => console.log(err));
+
+          Storage.get('example2.png', {
+            bucket: 'myapp-20181030214040-deployment'
+          })
+          .then(result => console.log(result))
+          .catch(err => console.log(err));
+          
+          
       }
     
       render() {
+          let me = Auth.currentAuthenticatedUser()
+          .then(user => console.log(user));
+
+          console.log(me);
+
           return (
             <div>
               <input
