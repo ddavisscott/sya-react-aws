f64f9eb80c87e723ae9332a731ecbcae845e369d
Kavan Samra <kssamra@ucsc.edu>
2018-11-05 16:29:10 -0800

    This one should work

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
+import AppBar from '@material-ui/core/AppBar';
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
