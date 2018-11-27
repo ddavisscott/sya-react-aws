72c845725b71d3714963c97fa9e25307c8d7e55c
Kavan <kssamra@ucsc.edu>
2018-11-27 01:16:54 -0800

    minor mod to params object

--- backend/updateArtistProfile.js
+++ backend/updateArtistProfile.js
@@ -5,21 +5,26 @@ const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east'});
 exports.handler = (event, context, callback) =>{
   //data holds all values passed into the body of the http request
   var data = JSON.parse(event.body);
+  //key is the users sub value, which is unique and used as the primary key 
+  //to the user table.
+  var key = event.queryStringParameters;
 
   var instagramLink = 'https://www.instagram.com/';
-  var facebookLink= 'https://www.facebook.com/';
+  var facebookLink = 'https://www.facebook.com/';
   var tumblrLink = 'https://www.tumblr.com/';
   var twitterLink = 'https://www.twitter.com/';
   
   var params = {
     TableName: 'artists',
-    Item:{
+    Key: key,
+    ExpressionAttributeValues:{
 
     }
   }
 
   if(data.instagram !== null && data.instagram !== undefined){
     instagramLink = instagramLink + data.instagram;
+
     params.Item["instagramLink"] = instagramLink;
   }
 
